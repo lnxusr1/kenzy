3feea7f839d7b3faeb13e93ae09905d1c90f6629
lnxusr1 <lnxusr1@kenzy.ai>
2022-12-24 08:28:41 -0500

    Fixes for RaspiPanel imports

--- src/kenzy/panels/__init__.py
+++ src/kenzy/panels/__init__.py
@@ -3,7 +3,7 @@ import sys
 import traceback
 
 try:
-    from .example_panel.panel import PanelApp as RaspiPanel
+    from .panel_v1.panel import PanelApp as RaspiPanel
 except Exception:
     logging.debug(str(sys.exc_info()[0]))
     logging.debug(str(traceback.format_exc()))
