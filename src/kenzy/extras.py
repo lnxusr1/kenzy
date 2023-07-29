e9746c95edb4418693cd3d34d5379376e2a2f8ac
lnxusr1 <lnxusr1@kenzy.ai>
2023-07-29 09:39:20 -0400

    STT model additions

--- src/kenzy/extras.py
+++ src/kenzy/extras.py
@@ -15,6 +15,37 @@ import json
 SSDP_DEVICE_TYPE = "urn:schemas-upnp-org:device:Kenzy-Core:1"
 
 
+def py_error_handler(filename, line, function, err, fmt):
+    """
+    Error handler to translate non-critical errors to logging messages.
+    
+    Args:
+        filename (str): Output file name or device (/dev/null).
+        line (int): Line number of error
+        function (str): Function containing
+        err (Exception): Exception raised for error
+        fmt (str): Format of log output
+    """
+
+    # Convert the parameters to strings for logging calls
+    fmt = fmt.decode("utf-8")
+    filename = filename.decode("utf-8")
+    fnc = function.decode('utf-8')
+    
+    # Setting up a logger so you can turn these errors off if so desired.
+    logger = logging.getLogger("CTYPES")
+
+    if (fmt.count("%s") == 1 and fmt.count("%i") == 1):
+        logger.debug(fmt % (fnc, line))
+    elif (fmt.count("%s") == 1):
+        logger.debug(fmt % (fnc))
+    elif (fmt.count("%s") == 2):
+        logger.debug(fmt % (fnc, str(err)))
+    else:
+        logger.debug(fmt)
+    return
+
+
 def get_raw_value(setting_value):
     if "." in setting_value and setting_value.replace(",", "").replace(".", "").isdigit():
         setting_value = float(setting_value.replace(",", ""))
--- src/kenzy/stt/core.py
+++ src/kenzy/stt/core.py
@@ -1,5 +1,4 @@
-# from ctypes import CFUNCTYPE, cdll, c_char_p, c_int
-from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+from ctypes import CFUNCTYPE, cdll, c_char_p, c_int
 import logging
 import queue
 import collections
@@ -10,41 +9,12 @@ import traceback
 import wave
 import io
 import soundfile
-from ctypes import CFUNCTYPE, cdll, c_char_p, c_int
-
-
-def py_error_handler(filename, line, function, err, fmt):
-    """
-    Error handler to translate non-critical errors to logging messages.
-    
-    Args:
-        filename (str): Output file name or device (/dev/null).
-        line (int): Line number of error
-        function (str): Function containing
-        err (Exception): Exception raised for error
-        fmt (str): Format of log output
-    """
-
-    # Convert the parameters to strings for logging calls
-    fmt = fmt.decode("utf-8")
-    filename = filename.decode("utf-8")
-    fnc = function.decode('utf-8')
-    
-    # Setting up a logger so you can turn these errors off if so desired.
-    logger = logging.getLogger("CTYPES")
-
-    if (fmt.count("%s") == 1 and fmt.count("%i") == 1):
-        logger.debug(fmt % (fnc, line))
-    elif (fmt.count("%s") == 1):
-        logger.debug(fmt % (fnc))
-    elif (fmt.count("%s") == 2):
-        logger.debug(fmt % (fnc, str(err)))
-    else:
-        logger.debug(fmt)
-    return
+from kenzy.extras import py_error_handler
 
 
 def speech_model(model_name="openai/whisper-tiny.en"):
+    from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+    
     processor = AutoProcessor.from_pretrained(model_name)
     model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name)
     model.config.forced_decoder_ids = None
