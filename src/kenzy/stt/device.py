10ab6d4be51153f74684da5331ebe1d6fdaa4935
lnxusr1 <lnxusr1@kenzy.ai>
2023-08-06 14:44:07 -0400

    Added restartability for STT

--- src/kenzy/stt/device.py
+++ src/kenzy/stt/device.py
@@ -21,6 +21,7 @@ class AudioProcessor:
     main_thread = None
     callback_thread = None
     callback_queue = None
+    restart_enabled = False
 
     def __init__(self, **kwargs):
         self.settings = kwargs
@@ -57,6 +58,7 @@ class AudioProcessor:
             logging.debug(str(traceback.format_exc()))
             self.logger.error("Unable to read from listener device.")
             self.stop_event.set()
+            self.restart_enabled = True
 
     def collect(self, data=None, context=None):
         self.logger.debug(f"{data}, {self.type}, {context.get()}")
@@ -68,10 +70,16 @@ class AudioProcessor:
         return False
     
     def start(self, **kwargs):
+        self.restart_enabled = False
 
         if self.is_alive():
             self.logger.error("Audio Processor already running")
             return KenzyErrorResponse("Audio Processor already running")
+        
+        if (self.main_thread is not None and self.main_thread.is_alive()) \
+                or (self.callback_thread is not None and self.callback_thread.is_alive()):
+
+            self.stop()
 
         self.callback_queue = queue.Queue()
         self.callback_thread = threading.Thread(target=self._process_callback, daemon=True)
@@ -88,15 +96,20 @@ class AudioProcessor:
             return KenzyErrorResponse("Unable to start Audio Processor")
         
     def stop(self, **kwargs):
-        if self.main_thread is None or not self.main_thread.is_alive():
+        if (self.main_thread is None or not self.main_thread.is_alive()) \
+                or (self.callback_thread is None or self.callback_thread.is_alive()):
+
             self.logger.error("Audio Processor is not running")
             return KenzyErrorResponse("Audio Processor is not running")
         
         self.stop_event.set()
-        self.main_thread.join()
 
-        self.callback_queue.put(None)
-        self.callback_thread.join()
+        if self.main_thread.is_alive():
+            self.main_thread.join()
+
+        if self.callback_thread.is_alive():
+            self.callback_queue.put(None)
+            self.callback_thread.join()
 
         if not self.is_alive():
             self.logger.info("Stopped Audio Processor")
